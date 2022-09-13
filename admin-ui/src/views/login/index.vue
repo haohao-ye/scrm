953e4788b26394486cca343ccac846da4f5efb03
zhanghaoyi <zhanghaoyi2000@qq.com>
2022-09-13 10:06:30 +0800

    修改登录页的输入框样式

--- admin-ui/src/views/login/index.vue
+++ admin-ui/src/views/login/index.vue
@@ -15,7 +15,7 @@
         <span class="svg-container">
           <svg-icon icon-class="user" />
         </span>
-        <el-input ref="username" v-model="loginForm.username" placeholder="Username" name="username" type="text"
+        <el-input ref="username" v-model="loginForm.username" placeholder="请输入用户名" name="username" type="text"
           tabindex="1" auto-complete="on" />
       </el-form-item>
 
@@ -24,7 +24,7 @@
           <svg-icon icon-class="password" />
         </span>
         <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType"
-          placeholder="Password" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
+          placeholder="请输入密码" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
         <span class="show-pwd" @click="showPwd">
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
@@ -122,6 +122,12 @@ export default {
 $bg: #283443;
 $light_gray: #fff;
 $cursor: #fff;
+$placeholder: #eae8e8;
+
+@font-face {
+  font-family: AliLight;
+  src: url('../../assets/font/Alibaba-PuHuiTi-Light.ttf')
+}
 
 @font-face {
   font-family: AliRegular;
@@ -141,8 +147,6 @@ $cursor: #fff;
 
 /* reset element-ui css */
 .login-container {
-  font-family: "AliRegular", "Source Han Sans CN", "Microsoft YaHei";
-
   .el-input {
     display: inline-block;
     height: 47px;
@@ -152,12 +156,20 @@ $cursor: #fff;
       background: transparent;
       border: 0px;
       font-size: 16px;
+      font-family: "AliRegular";
       -webkit-appearance: none;
       border-radius: 0px;
       padding: 12px 5px 12px 15px;
       color: $light_gray;
       height: 47px;
       caret-color: $cursor;
+
+      &::placeholder {
+        color: $placeholder;
+        font-size: 14px;
+        letter-spacing: 1px;
+        font-family: "AliLight";
+      }
 
       &:-webkit-autofill {
         box-shadow: 0 0 0px 1000px $bg inset !important;
@@ -168,7 +180,7 @@ $cursor: #fff;
 
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    background: rgba(37, 37, 37, 0.1);
     border-radius: 5px;
     color: #454545;
   }
@@ -177,7 +189,7 @@ $cursor: #fff;
 
 <style lang="scss" scoped>
 $bg: #2d3a4b;
-$dark_gray: #889aa4;
+$dark_gray: #eee;
 $light_gray: #eee;
 
 @font-face {
