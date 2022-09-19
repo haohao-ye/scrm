4d31f88a7b997ae4eb2765cedf99c24620989218
zhanghaoyi <zhanghaoyi2000@qq.com>
2022-09-19 15:04:17 +0800

    重新设计布局，并调整图表自适应窗口。

--- admin-ui/src/views/dashboard/components/LineChart.vue
+++ admin-ui/src/views/dashboard/components/LineChart.vue
@@ -1,77 +1,111 @@
 <template>
-    <div>
-        <div ref="lineChart" style="width: 800px; height: 400px; background-color: #ffffff; padding: 20px; border-radius: 20px;"></div>
-    </div>
+    <div :class="className" :style="{height:height,width:width}" />
 </template>
 
 <script>
-import * as echarts from 'echarts'
+import * as echarts from 'echarts'  // 根据 v5 修改引入方式
+import resize from './mixins/resize'
+
+const animationDuration = 6000
 
 export default {
+    mixins: [resize],
+    props: {
+        className: {
+            type: String,
+            default: 'chart'
+        },
+        width: {
+            type: String,
+            default: '100%'
+        },
+        height: {
+            type: String,
+            default: '400px'
+        }
+    },
+    data() {
+        return {
+            chart: null
+        }
+    },
     mounted() {
-        this.lineChart = echarts.init(this.$refs.lineChart)
-        this.lineChart.setOption({
-            title: {
-                text: '评价数据分析'
-            },
-            // 提示框
-            tooltip: {
-                trigger: 'axis'
-            },
-            // 图例
-            legend: {
-                icon: 'circle',
-                left: 'center',
-                top: 0,
-                data: ['好评', '一般', '差评']
-            },
-            grid: {
-                left: '3%',
-                right: '3%',
-                bottom: '3%',
-                containLabel: true
-            },
-            // 工具栏
-            toolbox: {
-                feature: {
-                    saveAsImage: {
-                        type: 'png'
-                    },
-                    magicType: {
-                        type: ['line', 'bar', 'stack']
-                    }
-                }
-            },
-            xAxis: {
-                type: 'category',
-                boundaryGap: false,
-                data: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [
-                {
-                    name: '好评',
-                    type: 'line',
-                    // smooth: true, // 平滑曲线显示
-                    data: [120, 132, 101, 134, 190, 230, 210, 201, 234, 290, 230, 210]
-                },
-                {
-                    name: '一般',
-                    type: 'line',
-                    // smooth: true,
-                    data: [100, 82, 91, 54, 90, 76, 110, 81, 104, 90, 130, 110]
-                },
-                {
-                    name: '差评',
-                    type: 'line',
-                    stack: '总量',
-                    // smooth: true,
-                    data: [10, 22, 21, 14, 19, 13, 20, 11, 34, 29, 20, 10]
-                }
-            ]
+        this.$nextTick(() => {
+            this.initChart()
         })
+    },
+    beforeDestroy() {
+        if (!this.chart) {
+            return
+        }
+        this.chart.dispose()
+        this.chart = null
+    },
+    methods: {
+        initChart() {
+            this.chart = echarts.init(this.$el)
+
+            this.chart.setOption({
+                title: {
+                    text: '评价数据分析'
+                },
+                // 提示框
+                tooltip: {
+                    trigger: 'axis'
+                },
+                // 图例
+                legend: {
+                    icon: 'circle',
+                    left: 'center',
+                    top: 0,
+                    data: ['好评', '一般', '差评']
+                },
+                grid: {
+                    left: '3%',
+                    right: '3%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                // 工具栏
+                toolbox: {
+                    feature: {
+                        saveAsImage: {
+                            type: 'png'
+                        },
+                        magicType: {
+                            type: ['line', 'bar', 'stack']
+                        }
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: '好评',
+                        type: 'line',
+                        // smooth: true, // 平滑曲线显示
+                        data: [120, 132, 101, 134, 190, 230, 210, 201, 234, 290, 230, 210]
+                    }, {
+                        name: '一般',
+                        type: 'line',
+                        // smooth: true,
+                        data: [100, 82, 91, 54, 90, 76, 110, 81, 104, 90, 130, 110]
+                    }, {
+                        name: '差评',
+                        type: 'line',
+                        stack: '总量',
+                        // smooth: true,
+                        data: [10, 22, 21, 14, 19, 13, 20, 11, 34, 29, 20, 10]
+                    }
+                ]
+            })
+        }
     }
 }
 </script>
